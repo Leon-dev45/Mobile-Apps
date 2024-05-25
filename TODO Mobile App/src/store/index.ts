c3d8c05ea70ed8993d181c31898188e37d64a7ae
Leon-dev45 <134835714+Leon-dev45@users.noreply.github.com>
2024-05-25 13:13:56 +0530

    Uploading storage

---
+++ TODO Mobile App/src/store/index.ts
@@ -0,0 +1,76 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface IGlobalStore {
+  categories: ICategory[];
+  tasks: ITask[];
+  addTask: (task: ITask) => void;
+  addCategory: (category: ICategory) => void;
+  updateCategory: (category: ICategory[]) => void;
+  updateTasks: (tasks: ITask[]) => void;
+  selectedCategory: null | ICategory;
+  updateSelectedCategory: (category: ICategory) => void;
+  toggleTaskStatus: (task: ITask) => void;
+}
+
+const useGlobalStore = create<IGlobalStore>()(
+  persist(
+    (set, get) => ({
+      categories: [],
+      tasks: [],
+      selectedCategory: null,
+      addTask: (task) => {
+        const { tasks } = get();
+        const updatedTasks = [...tasks, task];
+        set({
+          tasks: updatedTasks,
+        });
+      },
+      updateCategory: (category) => {
+        set({
+          categories: category,
+        });
+      },
+      updateTasks: (updatedTasks) => {
+        set({
+          tasks: updatedTasks,
+        });
+      },
+      updateSelectedCategory: (category) => {
+        set({
+          selectedCategory: category,
+        });
+      },
+      addCategory: (category) => {
+        const { categories } = get();
+        const updatedCategories = [...categories, category];
+        set({
+          categories: updatedCategories,
+        });
+      },
+      toggleTaskStatus: (task: ITask) => {
+        const { tasks } = get();
+        const updatedTasks = tasks.map((taskItem) => {
+          if (taskItem.id === task.id) {
+            return {
+              ...task,
+              completed: !task.completed,
+            };
+          } else {
+            return taskItem;
+          }
+        });
+        set({
+          tasks: updatedTasks,
+        });
+      },
+    }),
+    {
+      name: "todos-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
+
+export default useGlobalStore;
